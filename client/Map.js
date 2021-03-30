6583ef85033c37912f62210640853343b236cb64
Adnan Noori <noori.adnan@gmail.com>
2021-03-30 12:29:09 -0700

    - passed currentView down to Map

--- client/Map.js
+++ client/Map.js
@@ -3,27 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import RNLocation from 'react-native-location';
 
-const Map = () => {
+const Map = ({ currentView, updateCurrentView }) => {
 
   const [userLocation, setUserLocation] = useState({latitude: 0, longitude: 0});
 
-  RNLocation.configure({
-    distanceFilter: 5.0
-  })
-
-  RNLocation.requestPermission({
-    ios: "whenInUse",
-    android: {
-      detail: "coarse"
-    }
-  }).then(granted => {
-      if (granted) {
-        this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
-          setUserLocation({latitude: locations[0].latitude, longitude: locations[0].longitude})
-        })
-      }
+  useEffect(() => {
+    RNLocation.configure({
+      distanceFilter: 5.0
     })
 
+    RNLocation.requestPermission({
+      ios: "whenInUse",
+      android: {
+        detail: "coarse"
+      }
+    }).then(granted => {
+        if (granted) {
+          this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
+            setUserLocation({latitude: locations[0].latitude, longitude: locations[0].longitude})
+            updateCurrentView(locations[0])
+          })
+        }
+      })
+  }, [])
 
   return (
     <View style={styles.constainer}>
@@ -31,8 +33,8 @@ const Map = () => {
         style={styles.constainer}
         showsUserLocation={true}
         region={{
-          latitude: 37.78525,
-          longitude: -122.4124,
+          latitude: currentView.latitude,
+          longitude: currentView.longitude,
           latitudeDelta: 0.05,
           longitudeDelta: 0.0121,
         }}
