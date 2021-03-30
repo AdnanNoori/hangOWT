111a467c263fd89e0f0094e9ecb8562abae9e27d
Adnan Noori <noori.adnan@gmail.com>
2021-03-30 11:03:32 -0700

    - incorporated GooglePlacesAutoComplete

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import GetLocation from 'react-native-get-location';
+//import GetLocation from 'react-native-get-location';
 import RNLocation from 'react-native-location';
+import GooglePlacesInput from './client/GooglePlacesInput.js';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { googleKey } from './config.js';
 
 export default function App() {
 
@@ -27,7 +30,52 @@ export default function App() {
     })
 
   return (
-    <View style={styles.container}>
+    <View style={styles.constainer}>
+      <View style={styles.map}>
+        <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        region={{
+          latitude: 37.78525,
+          longitude: -122.4124,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.0121,
+        }}
+        >
+        <Marker
+          coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+          title={'Hello'}
+          description={'World'}
+          image={require('./marker.png')}
+        >
+        </Marker>
+        {console.log(userLocation)}
+        <Marker
+          coordinate={{ latitude : userLocation.latitude , longitude : userLocation.longitude }}
+          title={'Hello'}
+          description={'World'}
+          image={require('./marker.png')}
+        >
+        </Marker>
+      </MapView>
+     </View>
+     <GooglePlacesInput/>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  constainer: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+{/* <View style={styles.container}>
+      <Text style={styles.search}>HELLO</Text>
       <MapView
        style={styles.map}
        showsUserLocation={true}
@@ -54,84 +102,4 @@ export default function App() {
       >
       </Marker>
      </MapView>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 900,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
- });
-
-
- //step 1:
-{/* <MapView
-  style={styles.map}
-  region={{
-    latitude: 37.78525,
-    longitude: -122.4124,
-    latitudeDelta: 0.05,
-    longitudeDelta: 0.0121,
-}}
->
-</MapView> */}
-
-//step 2:
-//create marker
-{/* <Marker
-coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
-title={'Hello'}
-description={'World'}
->
-
-</Marker> */}
-
-//step 3:
-//marker draggable
-{/* <Marker draggable
-coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
-title={'Hello'}
-description={'World'}
->
-</Marker> */}
-
-//step 4:
-//marker image update
-{/* <Marker draggable
-coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
-title={'Hello'}
-description={'World'}
-image={require('./marker.png')}
->
-</Marker> */}
-
-//step 5:
-//show user location
-{/* <MapView
-  style={styles.map}
-  showsUserLocation={true}
-  region={{
-    latitude: 37.78525,
-    longitude: -122.4124,
-    latitudeDelta: 0.05,
-    longitudeDelta: 0.0121,
-  }}
->
-  <Marker
-    coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
-    title={'Hello'}
-    description={'World'}
-    image={require('./marker.png')}
-  >
-  </Marker>
-</MapView> */}
-
-
+    </View> */}
