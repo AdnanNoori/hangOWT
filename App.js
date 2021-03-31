39b1b59b2fe1a8837ba876d5b4f29438bb24a740
Adnan Noori <noori.adnan@gmail.com>
2021-03-30 17:25:42 -0700

    - Updated to include Modal

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-//import GetLocation from 'react-native-get-location';
-// import RNLocation from 'react-native-location';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import GooglePlacesInput from './client/GooglePlacesInput.js';
 import Map from './client/Map.js';
 import { getUserLocation } from './client/helperFunctions/helperFunctions.js';
@@ -13,12 +11,15 @@ export default function App() {
 
   const [userLocation, setUserLocation] = useState({latitude: 0, longitude: 0});
 
+  const [potentialEvent, setPotentialEvent] = useState(false)
+
   const updateCurrentView = (location) => {
     setCurrentView({latitude: location.latitude, longitude: location.longitude});
   }
 
   const resetView = () => {
     setCurrentView({latitude: userLocation.latitude, longitude: userLocation.longitude});
+    setPotentialEvent(false);
   }
 
   useEffect(() => {
@@ -31,11 +32,20 @@ export default function App() {
 
   return (
     <View style={styles.constainer}>
-      <Map currentView={currentView} userLocation={userLocation}/>
-      <GooglePlacesInput currentView={currentView} updateCurrentView={updateCurrentView}/>
-      <View>
+      <Map
+      potentialEvent={potentialEvent}
+      currentView={currentView}
+      userLocation={userLocation}/>
+      <View style={styles.searchInput}>
+        <GooglePlacesInput
+        currentView={currentView}
+        updateCurrentView={updateCurrentView}
+        setPotentialEvent={setPotentialEvent}
+        />
+      </View>
+      <View style={styles.resetButton}>
         <Button
-          title='click'
+          title='Reset'
           onPress={resetView}
         />
       </View>
@@ -43,8 +53,19 @@ export default function App() {
   );
 };
 
+var width = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   constainer: {
     ...StyleSheet.absoluteFillObject,
   },
+  resetButton: {
+    position: 'absolute',
+    right: 20,
+    top: 100
+  },
+  searchInput: {
+    position: 'absolute',
+    width: width
+  }
 });
