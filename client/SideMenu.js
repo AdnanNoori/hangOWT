4a6f1805f1c9c3bfbc03a6818e4e88c771b073f3
Adnan Noori <noori.adnan@gmail.com>
2021-03-31 10:37:48 -0700

    - Menu Bar

---
+++ client/SideMenu.js
@@ -0,0 +1,26 @@
+import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+const SideMenu = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
+      />
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+export default SideMenu;
